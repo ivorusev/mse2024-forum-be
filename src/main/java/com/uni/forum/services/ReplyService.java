8fd30463fe1b34dd6b5fce3728733363ca247e2d
stanislav1502 <stanislav1502@abv.bg>
2024-04-20 14:56:33 +0300

    added logger to services name-fixing function TODO: better function comments

--- src/main/java/com/uni/forum/services/ReplyService.java
+++ src/main/java/com/uni/forum/services/ReplyService.java
@@ -9,6 +9,8 @@ import com.uni.forum.repositories.ReplyPagingRepository;
 import com.uni.forum.repositories.ReplyRepository;
 import com.uni.forum.repositories.TopicRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
@@ -20,33 +22,61 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class ReplyService {
-    // TODO: Stanislav will review and fix (and break everything)
-
-
+    // TODO: Stanislav will review and fix (or break everything)
+    protected static final Logger logger = LogManager.getLogger();
 
     private final ReplyRepository replyRepository;
-    private final ReplyPagingRepository pagingRepository;
+    private final ReplyPagingRepository replyPagingRepository;
     private final TopicRepository topicRepository;
-    private final ReplyConverter converter;
+    private final ReplyConverter replyConverter;
 
-    public ReplyDto persist(ReplyDto dto) {
-        TopicEntity topicEntity = getTopicOrThrowException(dto.getTopicId());
-        ReplyEntity entity = converter.toEntity(dto);
+    /**
+     * Saves a new reply to the database
+     *
+     * @param reply data to be saved
+     * @return the saved reply
+     */
+    public ReplyDto persist(ReplyDto reply) {
+        logger.trace("Entered method ReplyService.persist()");
+
+        TopicEntity topicEntity = getTopicOrThrowException(reply.getTopicId());
+        ReplyEntity entity = replyConverter.toEntity(reply);
         entity.setTopic(topicEntity);
         ReplyEntity savedEntity = replyRepository.save(entity);
-        return converter.toDto(savedEntity);
+        ReplyDto savedReply = replyConverter.toDto(savedEntity);
+
+        logger.info("Added new Reply: " + savedReply.hashCode());
+        return savedReply;
     }
 
+    /**
+     * This
+     * @param id - topic
+     * @param page which page to be returned (starting from 0)
+     * @param pageSize number of items in the page
+     * @return
+     */
     public List<ReplyDto> getAllRepliesByTopic(Long id, Integer page, Integer pageSize) {
+        logger.trace("Entered method ReplyService.getAllRepliesByTopic()");
+
         TopicEntity topic = getTopicOrThrowException(id);
         PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.by("created").descending());
-        List<ReplyEntity> allByTopic = pagingRepository.findAllByTopic(topic, pageRequest);
-        return allByTopic.stream().map(converter::toDto).collect(Collectors.toList());
+        List<ReplyEntity> allRepliesByTopic = replyPagingRepository.findAllByTopic(topic, pageRequest);
+        List<ReplyDto> replies = allRepliesByTopic.stream().map(replyConverter::toDto).collect(Collectors.toList());
+
+        logger.info("Queried all replies for topic: " + topic.getTitle());
+        return replies;
     }
 
+    /**
+     * @param id
+     * @return
+     */
     private TopicEntity getTopicOrThrowException(Long id) {
+        logger.trace("Entered method ReplyService.getTopicOrThrowException()");
+
         Optional<TopicEntity> topic = topicRepository.findById(id);
-        if (topic.isEmpty()){
+        if (topic.isEmpty()) {
             throw new NonExistingEntityException();
         }
         return topic.get();
--- src/main/java/com/uni/forum/services/TopicService.java
+++ src/main/java/com/uni/forum/services/TopicService.java
@@ -6,8 +6,11 @@ import com.uni.forum.domain.entities.TopicEntity;
 import com.uni.forum.repositories.TopicPagingRepository;
 import com.uni.forum.repositories.TopicRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,28 +21,50 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class TopicService {
 
-    private final TopicRepository repository;
-    private final TopicConverter converter;
-    private final TopicPagingRepository pagingRepository;
+    protected static final Logger logger = LogManager.getLogger();
 
+    private final TopicRepository topicRepository;
+    private final TopicConverter topicConverter;
+    private final TopicPagingRepository topicPagingRepository;
+
+    /**
+     * @param topic
+     * @return
+     */
     public TopicDto persist(TopicDto topic) {
-        TopicEntity entity = converter.toEntity(topic);
-        TopicEntity savedEntity = repository.save(entity);
-        return converter.toDto(savedEntity);
+        logger.trace("Entered method TopicService.persist()");
+
+        TopicEntity entity = topicConverter.toEntity(topic);
+        TopicEntity savedEntity = topicRepository.save(entity);
+        return topicConverter.toDto(savedEntity);
     }
 
+    /**
+     * @param id
+     * @return
+     */
     public TopicDto getTopic(long id) {
+        logger.trace("Entered method TopicService.getTopic()");
+
         // TODO: don't use join, just query topic
-        Optional<TopicEntity> topic = repository.findById(id);
+        Optional<TopicEntity> topic = topicRepository.findById(id);
         if (topic.isEmpty()) {
-            throw new IllegalArgumentException(  "Topic not found: " + id);
+            throw new IllegalArgumentException("Topic not found: " + id);
         }
         TopicEntity topicEntity = topic.get();
-        return converter.toDto(topicEntity);
+        return topicConverter.toDto(topicEntity);
     }
 
+    /**
+     * @param page
+     * @param pageSize
+     * @return
+     */
     public List<TopicDto> getAllTopics(int page, int pageSize) {
-        Page<TopicEntity> all = pagingRepository.findAll(PageRequest.of(page, pageSize));
-        return all.getContent().stream().map(converter::toDto).collect(Collectors.toList());
+        logger.trace("Entered method TopicService.getAllTopics()");
+
+        PageRequest pageRequest = PageRequest.of(page, pageSize, Sort.by("created").descending());
+        Page<TopicEntity> allTopics = topicPagingRepository.findAll(pageRequest);
+        return allTopics.getContent().stream().map(topicConverter::toDto).collect(Collectors.toList());
     }
 }
